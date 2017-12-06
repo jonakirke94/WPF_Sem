651f2454be72e74bffefb8ad580bbb5c3fb3a400
swg-slaptop <swkgram@gmail.com>
2017-12-06 12:30:20 +0100

    added ListPage

---
+++ WPF_SmartList/Pages/ListPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_SmartList.AuthService;
+
+namespace WPF_SmartList.Pages
+{
+    /// <summary>
+    /// Interaction logic for ListPage.xaml
+    /// </summary>
+    public partial class ListPage : Page
+    {
+        private static List<User> UserList = new List<User>();
+        private static List<Item> ItemList = new List<Item>();
+
+        public ListPage(int listId)
+        {
+            InitializeComponent();
+            InitListValues(listId);
+        }
+
+        public void InitListValues(int listId)
+        {
+
+
+            UserListView = new ListView {ItemsSource = UserList};
+            ItemListView = new ListView {ItemsSource = ItemList};
+        }
+        
+    }
+}
